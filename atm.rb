584c6bb8ffa3548c6b024f40421abef4d1647840
Svetlana Filimonova <sfilimonova@me.com>
2014-02-19 13:56:58 +1100

    different notes

--- atm.rb
+++ atm.rb
@@ -1,10 +1,12 @@
 def withdraw(amount)
 
-  notes = [20,10,5]
+  notes = [50,20]
 
-  if amount <= 0 || amount % notes.min != 0
+
+  if amount <= 0
     return false
   end
+
   ##################################################
   #
   #             Your code goes here
@@ -13,14 +15,19 @@ def withdraw(amount)
 
   array = []
 
-  notes.each do |note|
+  notes.each_with_index do |note,index|
     number_of_notes = amount / note
+
+    next_note = notes[index+1]
+    number_of_notes -= 1 if next_note && ((amount % note)%next_note) != 0
+    
     number_of_notes.times do 
       array << note
       amount = amount - note
     end
+  
   end
-
+  return false if amount != 0
   array
 end
 
@@ -36,15 +43,18 @@ describe 'atm' do
     [-1, false],
     [0, false],
     [7, false],
-    [53, false],
-    [35, [20, 10, 5]],
-    [40, [20, 20]],
-    [65, [20, 20, 20, 5]],
-    [70, [20, 20, 20, 10]],
-    [75, [20, 20, 20, 10, 5]],
-  ].each do |input, expected|
-    it "should return #{expected} when $#{input} is withdrawn" do
-      withdraw(input).must_equal expected
+    [29, false],
+    [135, false],
+    [70, [50, 20]],
+    [80, [20, 20, 20, 20]],
+    [90, [50, 20, 20]],
+    [120, [50, 50, 20]],
+    [130, [50, 20, 20, 20, 20]],
+    [160, [50, 50, 20, 20, 20]],
+    [200, [50, 50, 50, 50]]
+  ].each do |amount, expected|
+    it "should return #{expected} when $#{amount} is withdrawn" do
+      withdraw(amount).must_equal expected
     end
   end
 end
